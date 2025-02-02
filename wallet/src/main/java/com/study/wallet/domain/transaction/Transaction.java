3a71009605de38c64daf7234e1d6a15451b80068
fernandes-felipe <felipefernandes101@hotmail.com>
2025-02-02 13:35:49 -0300

    refactor: adding boilerplate codes in Transaction Entity

--- wallet/src/main/java/com/study/wallet/domain/transaction/Transaction.java
+++ wallet/src/main/java/com/study/wallet/domain/transaction/Transaction.java
@@ -2,19 +2,13 @@ package com.study.wallet.domain.transaction;
 
 import com.study.wallet.domain.user.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity(name = "transactions")
-@Table(name="transactions")
-@Getter
-@Setter
-@AllArgsConstructor
+@Table(name= "transactions")
 @EqualsAndHashCode(of = "id")
 public class Transaction {
 
@@ -24,13 +18,53 @@ public class Transaction {
 
     private BigDecimal amount;
 
-    @ManyToMany
+    @ManyToOne
     @JoinColumn(name = "sender_id")
     private User sender;
 
-    @ManyToMany
+    @ManyToOne
     @JoinColumn(name = "receiver_id")
     private User receiver;
 
     private LocalDateTime timestemp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTimestemp() {
+        return timestemp;
+    }
+
+    public void setTimestemp(LocalDateTime timestemp) {
+        this.timestemp = timestemp;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
